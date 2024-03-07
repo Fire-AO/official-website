fa2848ec5f83fad85b8ee493b0ac3408b9f44878
Jehee Cheon <jeheecheon@gmail.com>
2024-03-07 21:45:10 +0900

    uninstalled unneccessary modules

--- projects/be/index.js
+++ projects/be/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from 'path';
 import dbConnect from "./config/dbConnect.js";
 import cors from "cors";
 
@@ -8,24 +7,15 @@ const server = express();
 
 server.use(cors());
 
-const __dirname = path.resolve();
-
-server.use(express.static(path.join(__dirname, '..','fe','dist')));
-
 //db연결
 dbConnect();
 
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-server.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'fe', 'dist', 'index.html'));
-});
-
 //apply 
 import applyRoutes from "./routes/applyRoutes.js";
 server.use("/api/apply", applyRoutes);
-
 
 server.listen(3000, ()=>{
     console.log('서버 실행 중');

5ecdde6f9dfcb81c7ef8d0b96af838ec7ac8754c
Jehee Cheon <jeheecheon@gmail.com>
2024-03-11 00:43:20 +0900

    removed 지원하기 button in Firstintro.tsx

--- projects/fe/src/pages/home/page/components/FirstIntro.tsx
+++ projects/fe/src/pages/home/page/components/FirstIntro.tsx
@@ -1,5 +1,4 @@
 import "@/pages/home/page/assets/css/FirstIntro.css";
-import { Link } from "react-router-dom";
 
 function FirstIntro() {
   return (
@@ -14,11 +13,6 @@ function FirstIntro() {
         </div>
         <div className="ex font-bold">
           <p>대학생활의 처음부터 끝까지 함께해 줄 Fire AO입니다.</p>
-        </div>
-        <div className="FIbutton ">
-          <Link className=" dark:bg-white font-semibold dark:font-normal bg-[rgb(13,192,222)] text-white dark:text-black" id="FIbut" to="/apply">
-            지원하러 가기
-          </Link>
         </div>
       </div>
       <div className="figure1"></div>

cce9129c496f1e668189c277f50e766ffe1ab253
Jehee Cheon <jeheecheon@gmail.com>
2024-03-11 03:23:46 +0900

    Revert "이제 안해요 절대"
    This reverts commit 4185bfb319e49d19a9820e79761618b6afa0602e.

--- projects/fe/src/common/components/InitialSetup.tsx
+++ projects/fe/src/common/components/InitialSetup.tsx
@@ -29,21 +29,19 @@ const InitialSetup = () => {
 
 
             {
-                <Helmet>
-                    isDarkMode
-                    ?<>
-                        <meta name='theme-color' content='#000000' />
-                        <meta name='apple-mobile-web-app-status-bar-style' content='#000000' />
-                        <meta name='msapplication-navbutton-color' content='#000000' />
-                        <meta name="theme-color" content="#000000" />
-                    </>
-                    :<>
+                isDarkMode
+                    ? <Helmet>
                         <meta name='theme-color' content='#ffffff' />
                         <meta name='apple-mobile-web-app-status-bar-style' content='#ffffff' />
                         <meta name='msapplication-navbutton-color' content='#ffffff' />
                         <meta name="theme-color" content="#ffffff" />
-                    </>
-                </Helmet>
+                    </Helmet>
+                    : <Helmet>
+                        <meta name='theme-color' content='#000000' />
+                        <meta name='apple-mobile-web-app-status-bar-style' content='#000000' />
+                        <meta name='msapplication-navbutton-color' content='#000000' />
+                        <meta name="theme-color" content="#000000" />
+                    </Helmet>
             }
         </>
     )

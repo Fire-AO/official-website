c07796173d633624d8472dba07d0d954933b7986
Jehee Cheon <jeheecheon@gmail.com>
2024-03-11 03:16:29 +0900

    그만제발

--- projects/fe/src/common/components/InitialSetup.tsx
+++ projects/fe/src/common/components/InitialSetup.tsx
@@ -34,11 +34,13 @@ const InitialSetup = () => {
                         <meta name='theme-color' content='#ffffff' />
                         <meta name='apple-mobile-web-app-status-bar-style' content='#ffffff' />
                         <meta name='msapplication-navbutton-color' content='#ffffff' />
+                        <meta name="theme-color" content="#ffffff" />
                     </Helmet>
                     : <Helmet>
                         <meta name='theme-color' content='#000000' />
                         <meta name='apple-mobile-web-app-status-bar-style' content='#000000' />
                         <meta name='msapplication-navbutton-color' content='#000000' />
+                        <meta name="theme-color" content="#000000" />
                     </Helmet>
             }
         </>

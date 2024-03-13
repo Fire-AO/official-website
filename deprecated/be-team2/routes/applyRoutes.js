231acce001332ed87d05ba4befedd58734f3a0b3
Jehee Cheon <jeheecheon@gmail.com>
2024-03-14 02:41:20 +0900

    Deleted deprecated folder

--- deprecated/be-team2/routes/applyRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import postApply from "../controllers/applyController.js";
-
-const router = express.Router();
-
-router.route("/")
-      .post(postApply);
-
-export default router;
